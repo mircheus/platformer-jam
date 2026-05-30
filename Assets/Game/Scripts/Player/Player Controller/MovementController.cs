15461299994f664f3aa201b2c6d42d4cba2c7b9f
Miroslav <mircheuss@yandex.ru>
2026-05-30 20:01:26 +0300

    Revert "remove jump_1"
    This reverts commit fc83c211b91d4548ee6f85aaa2f296e839a93d45.

--- Assets/Game/Scripts/Player/Player Controller/MovementController.cs
+++ Assets/Game/Scripts/Player/Player Controller/MovementController.cs
@@ -6,6 +6,15 @@ public class PlayerMovementController : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 5f;
 
+    [Header("Jump")]
+    [SerializeField] private float jumpForce = 12f;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundTrigger;
+    [SerializeField] private LayerMask groundLayer;
+
+    [SerializeField] private float groundCheckRadius = 0.2f;
+
     [Header("Performance")]
     [SerializeField] private InteractionSystemController interactionSystemController;
 
@@ -42,26 +51,15 @@ public class PlayerMovementController : MonoBehaviour
     {
         playerInput.actions["Move"].performed += OnMove;
         playerInput.actions["Move"].canceled += OnMove;
-        playerInput.actions["Interact"].performed += OnInteract;
+        playerInput.actions["Interact"].started += OnInteract;
     }
-    
     private void OnDisable()
     {
         playerInput.actions["Move"].performed -= OnMove;
         playerInput.actions["Move"].canceled -= OnMove;
         playerInput.actions["Interact"].performed -= OnInteract;
-    }
-
-    public void EnableMovement()
-    {
-        canMove = true;
-    }
-
-    public void DisableMovement()
-    {
-        canMove = false;
-    }
-
+    } 
+     
     private void OnMove(InputAction.CallbackContext context)
     {
         if (!canMove)
@@ -90,5 +88,26 @@ public class PlayerMovementController : MonoBehaviour
         {
             Application.targetFrameRate = -1;
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundTrigger == null)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawWireSphere(groundTrigger.position, groundCheckRadius);
+    }
+
+    public void EnableMovement()
+    {
+        canMove = true;
+    }
+    public void DisableMovement()
+    {
+        canMove = false;
     }
 }
