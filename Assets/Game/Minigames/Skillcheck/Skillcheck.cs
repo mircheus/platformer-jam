25a7c43a327547b88f5e6e9b9f5aee86994d7d3b
Miroslav <mircheuss@yandex.ru>
2026-06-01 19:55:12 +0300

    Add setup for Skillcheck game

---
+++ Assets/Game/Minigames/Skillcheck/Skillcheck.cs
@@ -0,0 +1,19 @@
+﻿using Minigames;
+using Minigames.Contract;
+using UnityEngine;
+
+namespace Game.Minigames.Skillcheck
+{
+    public class Skillcheck : MinigameBase
+    {
+        public override void Begin(MinigameContext ctx)
+        {
+            
+        }
+        
+        public void Win()
+        {
+            Complete(new MinigameResult());
+        }
+    }
+}
